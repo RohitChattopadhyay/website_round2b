38e9fe11ebf99f99b437a1f735d755411f22fe09
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2017-11-12 23:49:56 +0530

    Update ecellindex2.php

--- ecellindex2.php
+++ ecellindex2.php
@@ -1077,72 +1077,7 @@ $(window).scroll(function() {
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script>
-        <script type="text/javascript">
-        $(document).ready(function(){
-          $('.slider-single').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: true,
-            fade: false,
-            dots: true,
-            autoplay: true,
-            adaptiveHeight: true,
-            infinite: false,
-           useTransform: true,
-            speed: 400,
-            cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
-          });
-         
-          $('.slider-nav')
-            .on('init', function(event, slick) {
-              $('.slider-nav .slick-slide.slick-current').addClass('is-active');
-            })
-            .slick({
-              slidesToShow: 10,
-              slidesToScroll: 10,
-              dots: true,
-              arrows: true , 
-              autoplay: true , 
-              focusOnSelect: false,
-              infinite: false,
-              responsive: [{
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 5,
-                  slidesToScroll: 5,
-                }
-              }, {
-                breakpoint: 640,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 4,
-               }
-              }, {
-                breakpoint: 420,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-             }
-              }]
-            });
-         
-          $('.slider-single').on('afterChange', function(event, slick, currentSlide) {
-            $('.slider-nav').slick('slickGoTo', currentSlide);
-            var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
-            $('.slider-nav .slick-slide.is-active').removeClass('is-active');
-            $(currrentNavSlideElem).addClass('is-active');
-          });
-         
-          $('.slider-nav').on('click', '.slick-slide', function(event) {
-            event.preventDefault();
-            var goToSingleSlide = $(this).data('slick-index');
-         
-            $('.slider-single').slick('slickGoTo', goToSingleSlide);
-          });
-});
-	
-        </script>
+
         
 
         <script async defer
